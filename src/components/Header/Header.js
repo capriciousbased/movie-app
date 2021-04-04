481d1a967ee6daa49a17fbdb8f472c757f355f63
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-04 22:46:30 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function header() {
+  return (
+    <div>
+      <span className="header"> Entertainment Hub</span>
+    </div>
+  );
+}
+
+export default header;
