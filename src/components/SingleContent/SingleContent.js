4df114e555dee910514a2f49eb174cefc253a2a6
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-15 21:31:30 +0200

    contentmodel style added

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,6 +1,7 @@
+import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
 import "./SingleContent.css";
-import { Badge } from "@material-ui/core";
+import ContentModal from "../ContentModal/ContentModal";
 
 const SingleContent = ({
   id,
@@ -11,23 +12,22 @@ const SingleContent = ({
   vote_average,
 }) => {
   return (
-    <div className="media">
+    <ContentModal media_type={media_type} id={id}>
       <Badge
         badgeContent={vote_average}
         color={vote_average > 6 ? "primary" : "secondary"}
       />
-
       <img
         className="poster"
-        src={poster ? `${img_300}/${poster}` : unavailable}
+        src={poster ? `${img_300}${poster}` : unavailable}
         alt={title}
       />
       <b className="title">{title}</b>
       <span className="subTitle">
-        {media_type === "tv" ? "tv_series" : "movie"}
-        <span className="subtitle">{date}</span>
+        {media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
